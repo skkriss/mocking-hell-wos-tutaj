30bdbbf77cd61490c5d1ca73dcd439f7b4913037
wkorol <wiktorkorol@icloud.com>
2018-01-07 14:27:01 +0100

    Update of User class

---
+++ lib/user.rb
@@ -0,0 +1,22 @@
+require 'date'
+
+class User
+  attr_reader :id, :login, :first_name, :last_name, :pesel, :birthday
+
+  def initialize(id, login, first_name, last_name, pesel, birthday)
+    @id = id
+    @login = login
+    @first_name = first_name
+    @last_name = last_name
+    raise ArgumentError, 'Pesel is not valid' unless
+    User.pesel_check(pesel)
+    @pesel = pesel
+
+  end
+
+  def self.pesel_is_valid(pesel)
+    return false if pesel.to_s.length != 11
+    true
+  end
+
+end
